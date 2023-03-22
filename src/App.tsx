4ed4c3caf4f3e15ff2925c1ae775c86a18222f10
Thalys Menezes <thalys.tauan@ulbra.inf.br>
2023-03-22 03:08:23 -0300

    changes to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { List } from './List'
+import { Header } from './components/Header'
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <List
         author="Jhon"
         content="lorem"
