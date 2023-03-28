caed84cc3ebbffce4462c21ccc21763b4fa0e269
Thalys Menezes <thalys.tauan@ulbra.inf.br>
2023-03-28 20:49:46 -0300

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import styles from './Header.module.css'
 
+import rocket from '../assets/rocket.svg';
+import todologo from '../assets/todo.svg';
+
 export function Header() {
   return(
       <header className={styles.header}> 
-         <strong> Todo List </strong>
+      
+         <img src={rocket} alt="Logotipo Rocket" />
+         <img src={todologo} alt="Logotipo To Do" />
+      
       </header>
     );
 }
